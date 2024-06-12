61f833e274983270c99209d94d8b68ce126288f2
kazem-web-project <kaz.gh22@gmail.com>
2024-06-12 20:22:20 +0200

    minor changes for linux

--- build_and_up.sh
+++ build_and_up.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Change directory to frontend_blue
-cd frontend_blue || exit
+#cd ./frontend_blue || exit
 # Build the Docker image for frontend_blue
-docker build . --tag cooopeir/conint_frontend:0.2
+docker build ./frontend_blue --tag cooopeir/conint_frontend:0.2
 # Change back to the root directory
-cd ..
+#cd ..
 
 # Change directory to frontend_green
-cd frontend_green || exit
+#cd frontend_green || exit
 # Build the Docker image for frontend_green
-docker build . --tag cooopeir/conint_frontend:0.1
+docker build ./frontend_green --tag cooopeir/conint_frontend:0.1
 # Change back to the root directory
-cd ..
+#cd ..
 
 # Bring up the Docker Compose services
 docker compose up -d
