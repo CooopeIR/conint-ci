62b6b1cb4b2a02578faeaafd664db97f7ba29576
Cooopeir <cooopeir@LollesLappy>
2024-06-16 15:43:48 +0200

    added first gitlab ci config

--- build_and_up.sh
+++ build_and_up.sh
@@ -3,14 +3,14 @@
 # Change directory to frontend_blue
 #cd ./frontend_blue || exit
 # Build the Docker image for frontend_blue
-docker build ./frontend_blue --tag cooopeir/conint_frontend:0.2
+docker build ./frontend_blue --tag cooopeir/conint_frontend:blue
 # Change back to the root directory
 #cd ..
 
 # Change directory to frontend_green
 #cd frontend_green || exit
 # Build the Docker image for frontend_green
-docker build ./frontend_green --tag cooopeir/conint_frontend:0.1
+docker build ./frontend_green --tag cooopeir/conint_frontend:green
 # Change back to the root directory
 #cd ..
 
--- run_windows.bat
+++ run_windows.bat
@@ -2,14 +2,14 @@
 REM Change directory to frontend_blue
 cd frontend_blue
 REM Build the Docker image for frontend_blue
-docker build . --tag cooopeir/conint_frontend:0.2
+docker build . --tag cooopeir/conint_frontend:blue
 REM Change back to the root directory
 cd ..
 
 REM Change directory to frontend_green
 cd frontend_green
 REM Build the Docker image for frontend_green
-docker build . --tag cooopeir/conint_frontend:0.1
+docker build . --tag cooopeir/conint_frontend:green
 REM Change back to the root directory
 cd ..
 
