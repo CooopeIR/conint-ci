b1f65d71d2b35b1ab2cfdfbe49d9185cfafdaf85
root <cooopeir@LollesLappy>
2024-06-17 16:29:05 +0200

    connection striong hardcoded

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,13 +1,21 @@
 const { Sequelize } = require('sequelize');
 
+// const connUrl =
+//     process.env.DB_DIALECT + '://' +
+//     process.env.DB_USER + ':' +
+//     process.env.DB_PW + '@' +
+//     process.env.DB_HOST + '/' +
+//     process.env.DB_NAME
+
 const connUrl =
-    process.env.DB_DIALECT + '://' +
-    process.env.DB_USER + ':' +
-    process.env.DB_PW + '@' +
-    process.env.DB_HOST + '/' +
-    process.env.DB_NAME;
+    'mariadb' + '://' +
+    'todo-admin' + ':' +
+    'admin-pw' + '@' +
+    'mariadb-prod.c1gwkiqyc9gp.us-east-1.rds.amazonaws.com' + '/' +
+    'tododot';
 
 const db = new Sequelize(connUrl);
+
 
 const models = [
     require('../models/todo'),
